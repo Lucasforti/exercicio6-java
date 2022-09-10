2b0d5b4ccf152fcbb3734a40e0e5f37c0f67fdb3
Lucas Forti <98174630+Lucasforti@users.noreply.github.com>
2022-09-10 12:44:34 -0300

    Add files via upload

---
+++ ex6.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex6 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double a, b, c, triangulo, circulo, trapezio, quadrado, retangulo;
+
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		c = sc.nextDouble();
+		
+		triangulo = a * c / 2;
+		circulo = c * c * 3.14159;
+		trapezio = (a + b) * c / 2;
+		quadrado = b * b;
+		retangulo = a * b;
+		
+		System.out.printf("TRIANGULO: %.3f%n", triangulo);
+		System.out.printf("CIRCULO: %.3f%n", circulo);
+		System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+		System.out.printf("QUADRADO: %.3f%n", quadrado);
+		System.out.printf("RETANGULO: %.3f%n", retangulo);
+		
+		sc.close();
+	}
+
+}
